c25c31b6779211779320010d4c21bb30bb96acf1
Axel Sorenson <axelpsorenson@gmail.com>
2023-09-27 14:00:14 -0700

    Added Welcome component

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,12 +1,10 @@
 import './index.css'
+import Welcome from '../Welcome';
 
 export default function Home() {
   return (
-    <>
-      <div className="content">
-        <h1>Welcome to the Home Page</h1>
-        <p>This is the home page content.</p>
-      </div>
-    </>
+    <div className="welcome">
+      <Welcome />
+    </div>
   );
 }
---
+++ src/components/Welcome/index.tsx
@@ -0,0 +1,18 @@
+import './index.scss'
+import Portrait from '../../assets/img/Axel.jpg'
+
+export default function Welcome() {
+  return (
+    <div className="container">
+      <div className="portrait">
+        <img className="portrait-image" src={Portrait} alt="Axel Sorenson" />
+      </div>
+      <div className="text">
+        <h1>Hello World! <br />
+          I'm Axel Sorenson.
+        </h1>
+        <h2>Student at University of California, Irvine</h2>
+      </div>
+    </div>
+  );
+}
