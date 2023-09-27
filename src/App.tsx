9ade36b0f6ef868842247e99855c0122dcc63f2c
Axel Sorenson <axelpsorenson@gmail.com>
2023-09-27 10:45:59 -0700

    Testing Routes with React

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,15 @@
+import './App.css';
+import {Routes, Route} from 'react-router-dom';
+import Home from './components/Home';
+import Contact from './components/Contact';
+
 export default function App() {
-  return ("Hello World")
+  return ( 
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </>
+  )
 }
