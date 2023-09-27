25505c0843a3f3ee0b471335c953f10fb47f2607
Axel Sorenson <axelpsorenson@gmail.com>
2023-09-27 13:11:32 -0700

    Added a logo to the navbar

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -1,9 +1,13 @@
-import './index.css'
+import './index.scss'
 import { Link } from 'react-router-dom';
+import Logo from '../../assets/img/logo.png'
 
 export default function Navigation() {
   return (
     <div className = 'topnav'>
+      <Link to="/" className = 'logo'>
+        <img src = {Logo} alt="Logo"/>
+      </Link>
         <Link to="/">Home</Link>
         <Link to="/contact">Contact</Link>
     </div>
