e8e80d14d9d20da22d7cbb97ea514d1fb7148e5e
Axel Sorenson <axelpsorenson@gmail.com>
2023-09-27 11:47:28 -0700

    Learned about and implemented Links

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,8 +1,13 @@
+import { Link } from 'react-router-dom';
+
 export default function Home() {
   return (
     <div>
       <h1>Welcome to the Home Page</h1>
-      <p>This is the home page content.</p>
+      <p>
+      This is the home page content. You can{' '}
+        <Link to="/contact">click here</Link> to go to another part of the website.
+      </p>
     </div>
   );
 }
